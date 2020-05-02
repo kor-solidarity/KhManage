ffe844f8b2d090f4438f81664cb0beb087a5dce9
apple <apple@apple-ui-air>
2020-05-02 18:54:44 +0900

    대시보드 작업중

--- src/main/java/com/kh/manage/member/model/vo/MyStatic.java
+++ src/main/java/com/kh/manage/member/model/vo/MyStatic.java
@@ -1,14 +1,21 @@
 package com.kh.manage.member.model.vo;
 
 import java.sql.Date;
+import java.util.ArrayList;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.kh.manage.work.model.vo.WorkAttachment;
 
 public class MyStatic implements java.io.Serializable{
-	
+
 	private String workNo;
 	private String workName;
 	private String status;
 	private String projectNo;
+	private String projectName;
+	@JsonFormat(pattern="yyyy-MM-dd")
 	private Date beginDate;
+	@JsonFormat(pattern="yyyy-MM-dd")
 	private Date completeDate;
 	private String precedeNo;
 	private String completeRate;
@@ -19,18 +26,29 @@ public class MyStatic implements java.io.Serializable{
 	private String workType;
 	private String memberNo;
 	private String workStatus;
-	private String memberName;
-	
+	private ArrayList<WorkAttachment> workAttachment;
+	//'시작전', '개발중', '개발완료', '테스트완료', 'PL검토중', 'PL검토완료', '개발지연'
+	private int before;
+	private int developing;
+	private int completed;
+	private int testCompleted;
+	private int plChecking;
+	private int plChecked;
+	private int delayed;
+
 	public MyStatic() {}
 
-	public MyStatic(String workNo, String workName, String status, String projectNo, Date beginDate, Date completeDate,
-			String precedeNo, String completeRate, String grantorNo, String workLevel, String highWorkNo, String memo,
-			String workType, String memberNo, String workStatus, String memberName) {
+	public MyStatic(String workNo, String workName, String status, String projectNo, String projectName, Date beginDate,
+			Date completeDate, String precedeNo, String completeRate, String grantorNo, String workLevel,
+			String highWorkNo, String memo, String workType, String memberNo, String workStatus,
+			ArrayList<WorkAttachment> workAttachment, int before, int developing, int completed, int testCompleted,
+			int plChecking, int plChecked, int delayed) {
 		super();
 		this.workNo = workNo;
 		this.workName = workName;
 		this.status = status;
 		this.projectNo = projectNo;
+		this.projectName = projectName;
 		this.beginDate = beginDate;
 		this.completeDate = completeDate;
 		this.precedeNo = precedeNo;
@@ -42,7 +60,14 @@ public class MyStatic implements java.io.Serializable{
 		this.workType = workType;
 		this.memberNo = memberNo;
 		this.workStatus = workStatus;
-		this.memberName = memberName;
+		this.workAttachment = workAttachment;
+		this.before = before;
+		this.developing = developing;
+		this.completed = completed;
+		this.testCompleted = testCompleted;
+		this.plChecking = plChecking;
+		this.plChecked = plChecked;
+		this.delayed = delayed;
 	}
 
 	public String getWorkNo() {
@@ -75,6 +100,14 @@ public class MyStatic implements java.io.Serializable{
 
 	public void setProjectNo(String projectNo) {
 		this.projectNo = projectNo;
+	}
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
 	}
 
 	public Date getBeginDate() {
@@ -165,24 +198,82 @@ public class MyStatic implements java.io.Serializable{
 		this.workStatus = workStatus;
 	}
 
-	public String getMemberName() {
-		return memberName;
+	public ArrayList<WorkAttachment> getWorkAttachment() {
+		return workAttachment;
 	}
 
-	public void setMemberName(String memberName) {
-		this.memberName = memberName;
+	public void setWorkAttachment(ArrayList<WorkAttachment> workAttachment) {
+		this.workAttachment = workAttachment;
+	}
+
+	public int getBefore() {
+		return before;
+	}
+
+	public void setBefore(int before) {
+		this.before = before;
+	}
+
+	public int getDeveloping() {
+		return developing;
+	}
+
+	public void setDeveloping(int developing) {
+		this.developing = developing;
+	}
+
+	public int getCompleted() {
+		return completed;
+	}
+
+	public void setCompleted(int completed) {
+		this.completed = completed;
+	}
+
+	public int getTestCompleted() {
+		return testCompleted;
+	}
+
+	public void setTestCompleted(int testCompleted) {
+		this.testCompleted = testCompleted;
+	}
+
+	public int getPlChecking() {
+		return plChecking;
+	}
+
+	public void setPlChecking(int plChecking) {
+		this.plChecking = plChecking;
+	}
+
+	public int getPlChecked() {
+		return plChecked;
+	}
+
+	public void setPlChecked(int plChecked) {
+		this.plChecked = plChecked;
+	}
+
+	public int getDelayed() {
+		return delayed;
+	}
+
+	public void setDelayed(int delayed) {
+		this.delayed = delayed;
 	}
 
 	@Override
 	public String toString() {
 		return "MyStatic [workNo=" + workNo + ", workName=" + workName + ", status=" + status + ", projectNo="
-				+ projectNo + ", beginDate=" + beginDate + ", completeDate=" + completeDate + ", precedeNo=" + precedeNo
-				+ ", completeRate=" + completeRate + ", grantorNo=" + grantorNo + ", workLevel=" + workLevel
-				+ ", highWorkNo=" + highWorkNo + ", memo=" + memo + ", workType=" + workType + ", memberNo=" + memberNo
-				+ ", workStatus=" + workStatus + ", memberName=" + memberName + "]";
+				+ projectNo + ", projectName=" + projectName + ", beginDate=" + beginDate + ", completeDate="
+				+ completeDate + ", precedeNo=" + precedeNo + ", completeRate=" + completeRate + ", grantorNo="
+				+ grantorNo + ", workLevel=" + workLevel + ", highWorkNo=" + highWorkNo + ", memo=" + memo
+				+ ", workType=" + workType + ", memberNo=" + memberNo + ", workStatus=" + workStatus
+				+ ", workAttachment=" + workAttachment + ", before=" + before + ", developing=" + developing
+				+ ", completed=" + completed + ", testCompleted=" + testCompleted + ", plChecking=" + plChecking
+				+ ", plChecked=" + plChecked + ", delayed=" + delayed + "]";
 	}
 	
-	
-	
-	
+
+
 }
